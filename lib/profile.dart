afd3aa32b0a133157759a43846bfd77fb43494e1
Savely00001 <131238511+Savely00001@users.noreply.github.com>
2026-04-12 15:09:23 +0300

    Update profile.dart

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,1 +1,281 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Филин",
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            //Верхняя часть
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    // Шапка с логотипом
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        children: [
+                          Image.asset('assets/logo.png', width: 60, height: 60),
+                          const SizedBox(width: 10),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text("Филин", style: TextStyle(fontSize: 24)),
+                                Text(
+                                  "Проект по поиску помощи в проектах",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.black,
+                                  ),
+                                  softWrap: true,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    const Padding(
+                      padding: EdgeInsets.only(top: 10, left: 30),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Мой профиль",
+                          style: TextStyle(fontSize: 32),
+                        ),
+                      ),
+                    ),
+
+                    Container(
+                      width: screenWidth * 0.85,
+                      margin: const EdgeInsets.only(top: 25),
+                      padding: const EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(11),
+                        border: Border.all(
+                          color: const Color.fromRGBO(199, 199, 199, 1),
+                          width: 1,
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.account_circle,
+                            size: 42,
+                            color: Color.fromRGBO(33, 173, 252, 1),
+                          ),
+                          const SizedBox(width: 10),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: const [
+                                  Text(
+                                    "Иванов Иван",
+                                    style: TextStyle(fontSize: 14),
+                                  ),
+                                  SizedBox(width: 10),
+                                  Text(
+                                    "251-222",
+                                    style: TextStyle(fontSize: 14),
+                                  ),
+                                ],
+                              ),
+                              const Text(
+                                "Информационные технологии",
+                                style: TextStyle(fontSize: 14),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    const Padding(
+                      padding: EdgeInsets.only(top: 30, left: 30, bottom: 20),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Выбранное направление ВКР/Проекта:",
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: BottonText(text: "Нажмите, чтобы начать ввод"),
+                    ),
+
+                    const Padding(
+                      padding: EdgeInsets.only(top: 30, left: 30, bottom: 20),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Оставить отзыв на преподавателя:",
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: BottonText(
+                        text: "Нажмите, чтобы выбрать из списка",
+                      ),
+                    ),
+
+                    const Padding(
+                      padding: EdgeInsets.only(top: 30, left: 30, bottom: 20),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Посмотреть мои отзывы:",
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: BottonText(
+                        text: "Нажмите, чтобы выбрать из списка",
+                      ),
+                    ),
+
+                    const SizedBox(height: 50),
+                  ],
+                ),
+              ),
+            ),
+            //Нижняя панель
+            Container(
+              width: double.infinity, //Растягиваем на всю ширину экрана
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(199, 199, 199, 1),
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(color: Colors.white, width: 1),
+              ),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: ButtonMain(
+                      text: "Входящие",
+                      icon: Icons.sms,
+                      hasBack: false,
+                    ),
+                  ),
+                  Expanded(
+                    child: ButtonMain(
+                      text: "Преподаватели",
+                      icon: Icons.auto_stories,
+                      hasBack: false,
+                    ),
+                  ),
+                  Expanded(
+                    child: ButtonMain(
+                      text: "Профиль",
+                      icon: Icons.person,
+                      hasBack: true,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BottonText extends StatelessWidget {
+  final String text;
+  const BottonText({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        width: screenWidth * 0.85,
+        padding: const EdgeInsets.all(17),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(11),
+          border: Border.all(
+            color: const Color.fromRGBO(199, 199, 199, 1),
+            width: 1,
+          ),
+        ),
+        child: Center(child: Text(text, style: const TextStyle(fontSize: 13))),
+      ),
+    );
+  }
+}
+
+class ButtonMain extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final bool hasBack;
+  const ButtonMain({
+    super.key,
+    required this.text,
+    required this.icon,
+    required this.hasBack,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: hasBack ? Color.fromRGBO(33, 173, 252, 1) : Colors.transparent,
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, color: Colors.white),
+            const SizedBox(height: 5),
+            Text(
+              text,
+              style: const TextStyle(fontSize: 10, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
